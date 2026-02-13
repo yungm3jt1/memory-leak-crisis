321d3d87597adf74e9ee7928aaafb377744ac91d
read-docs <u69_tymmos_lbn@technischools.com>
2026-02-13 11:58:19 +0100

    feat: backend

---
+++ backend/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBedxHV-3hn-kYrTaB5zFFTYCTmEYhw6Uk",
+  authDomain: "dihh-9fbd3.firebaseapp.com",
+  databaseURL: "https://dihh-9fbd3-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "dihh-9fbd3",
+  storageBucket: "dihh-9fbd3.firebasestorage.app",
+  messagingSenderId: "62288382247",
+  appId: "1:62288382247:web:7e8219aba86f3d27268102"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { app, db };
